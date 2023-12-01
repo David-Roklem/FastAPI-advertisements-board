af1bd5d290e5b79f261014b72cd407efe112eebb
David-Roklem <roklem@mail.ru>
2023-12-01 20:33:21 +0300

    fix. Add DB session maker

--- src/core/db.py
+++ src/core/db.py
@@ -1,12 +1,14 @@
-from sqlalchemy.ext.asyncio import (
-    create_async_engine, async_sessionmaker, AsyncSession
-)
+from typing import AsyncGenerator
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
 from src.core.config import settings
 
 async_engine = create_async_engine(
     settings.db_url_asyncpg, future=True, echo=True
 )
-async_session_maker = async_sessionmaker(
-    async_engine, class_=AsyncSession, expire_on_commit=False
-)
+async_session_maker = async_sessionmaker(async_engine, expire_on_commit=False)
+
+
+async def get_async_session() -> AsyncGenerator:
+    async with async_session_maker() as session:
+        yield session
