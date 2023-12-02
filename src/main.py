0d12d78fa79ebdde542a501baba199abbad8f68f
David-Roklem <roklem@mail.ru>
2023-12-02 13:55:29 +0300

    Add pydantic-email validation dependency

--- src/main.py
+++ src/main.py
@@ -3,8 +3,10 @@ import uvicorn
 from fastapi import FastAPI
 
 from core.config import settings
+from api.users.user import router as user_router
 
 app = FastAPI(title=settings.app_title)
+app.include_router(user_router)
 
 if __name__ == '__main__':
     uvicorn.run('main:app', reload=True)
