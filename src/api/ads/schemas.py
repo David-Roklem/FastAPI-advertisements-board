ae80b702ec134f293c7975afea734787981d9c78
David-Roklem <roklem@mail.ru>
2023-12-02 18:47:20 +0300

    Add schemas for ads

---
+++ src/api/ads/schemas.py
@@ -0,0 +1,18 @@
+from pydantic import UUID4, BaseModel, ConfigDict, Field
+
+
+class AdBase(BaseModel):
+    title: str = Field(..., min_length=3, max_length=150)
+    description: str = Field(..., min_length=100, max_length=1000)
+    type: str = Field(..., min_length=3, max_length=150)
+
+
+class CreateAd(AdBase):
+    pass
+
+
+class Ad(AdBase):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: UUID4
+    user_id: UUID4
