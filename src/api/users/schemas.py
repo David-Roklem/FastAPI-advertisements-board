8c5e66c0959452778ebe3d5e8eb14a1684671e00
David-Roklem <roklem@mail.ru>
2023-12-02 13:56:28 +0300

    Add pydantic schemas for User

---
+++ src/api/users/schemas.py
@@ -0,0 +1,18 @@
+from pydantic import UUID4, BaseModel, ConfigDict, EmailStr, Field
+
+
+class UserBase(BaseModel):
+    username: str = Field(..., min_length=3, max_length=50)
+    email: EmailStr = None
+    password: str
+
+
+class CreateUser(UserBase):
+    pass
+
+
+class User(UserBase):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: UUID4
+    is_admin: bool = False
