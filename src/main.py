018c346104130284f2ff9533630cceb0361bba68
David-Roklem <roklem@mail.ru>
2023-12-02 18:46:17 +0300

    Include ads_router

--- src/main.py
+++ src/main.py
@@ -4,9 +4,11 @@ from fastapi import FastAPI
 
 from core.config import settings
 from api.users.user import router as user_router
+from api.ads.ad import router as ads_router
 
 app = FastAPI(title=settings.app_title)
 app.include_router(user_router)
+app.include_router(ads_router)
 
 if __name__ == '__main__':
     uvicorn.run('main:app', reload=True)
